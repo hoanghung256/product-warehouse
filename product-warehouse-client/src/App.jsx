e7465f8fcd819fe6ba985d28fb990765be74ef16
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-02-27 11:26:50 +0700

    Implement react router

--- product-warehouse-client/src/App.jsx
+++ product-warehouse-client/src/App.jsx
@@ -1,12 +1,23 @@
 import Products from "./pages/Products";
-import "./App.css";
+import ProductDetail from "./pages/ProductDetail";
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [productDetail, setProductDetail] = useState(null);
+
   return (
     <div className="App">
-      <div className="App-header">
-        <Products />
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={<Products setProductDetail={setProductDetail} />}
+        />
+        <Route
+          path="/products/:productId"
+          element={<ProductDetail product={productDetail} />}
+        />
+      </Routes>
     </div>
   );
 }
