325d1529c059fac53870649169a54300fa8cc383
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-02-29 23:15:17 +0700

    Update response message through StatusDto, bug at doPut()

--- product-warehouse-server/src/main/java/com/app/controllers/ProductController.java
+++ product-warehouse-server/src/main/java/com/app/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.app.controllers;
 
 import com.app.Configuration;
 import com.app.models.Product;
+import com.app.models.StatusDto;
 import com.app.utils.database.ProductDAO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -17,16 +18,18 @@ import java.util.logging.Logger;
  *
  * @author hoang hung
  */
-@WebServlet(name = "ProductController", urlPatterns = {"/api/products"})
-public class ProductController extends  HttpServlet {
+@WebServlet(name = "ProductController", urlPatterns = { "/api/products" })
+public class ProductController extends HttpServlet {
     private static final ProductDAO dbContext = Configuration.products;
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
         int deleteId = Integer.parseInt(req.getParameter("id"));
-        if (dbContext.delete(deleteId)) {
-            out.print("Delete succeed");
+        if (dbContext.delete(deleteId) == 1) {
+            out.print(new StatusDto(0, "Delete successfully!"));
+        } else {
+            out.print(new StatusDto(1, "Delete failed!"));
         }
     }
 
@@ -37,11 +40,14 @@ public class ProductController extends  HttpServlet {
             String name = req.getParameter("name");
             int quantity = Integer.parseInt(req.getParameter("quantity"));
             int price = Integer.parseInt(req.getParameter("price"));
-            
-            dbContext.add(name, quantity, price);
-            out.print("Create new product succeed");
+
+            if (dbContext.add(name, quantity, price) == 1) {
+                out.print(new StatusDto(0, "Add product successfully!"));
+            } else {
+                out.print(new StatusDto(1, "Add product failed!"));
+            }
         } catch (NumberFormatException e) {
-            out.print("Create new product failed");
+            out.print(new StatusDto(1, "Add product failed!"));
             Logger.getLogger(ProductController.class.getName()).log(Level.SEVERE, null, e);
         }
     }
@@ -49,37 +55,27 @@ public class ProductController extends  HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
-        
+
         out.print(dbContext.getAll());
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
         try {
             int id = Integer.parseInt(req.getParameter("id"));
-            Product p = dbContext.get(id);
-            
+            String name = req.getParameter("name");
+            int quantity = Integer.parseInt(req.getParameter("quantity"));
+            int price = Integer.parseInt(req.getParameter("price"));
+            Product p = new Product(id, name, quantity, price);
 
-            String[] parameters = {"name", "quantity", "price", "image"};
-            for (String parameter : parameters) {
-                String value = req.getParameter(parameter);
-                if (value != null) {
-                    switch (parameter) {
-                        case "name":
-                            p.setName(value);
-                            break;
-                        case "quantity":
-                            p.setQuantity(Integer.parseInt(value));
-                            break;
-                        case "price":
-                            p.setPrice(Integer.parseInt(value));
-                            break;
-                    }
-                }
+            if (dbContext.update(p) == 1) {
+                out.print(new StatusDto(0, "Update product successfully!"));
+            } else {
+                out.print(new StatusDto(1, "Update product failed!"));
             }
-
-            dbContext.update(p);
         } catch (NumberFormatException e) {
+            out.print(new StatusDto(1, "Update product failed!"));
             Logger.getLogger(ProductController.class.getName()).log(Level.SEVERE, null, e);
         }
     }
