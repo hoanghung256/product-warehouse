35ba933ebc5cf770d0192a62641cecfb579169e8
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-02-29 23:19:34 +0700

    Refactor implementation for router

--- product-warehouse-client/src/App.jsx
+++ product-warehouse-client/src/App.jsx
@@ -1,8 +1,8 @@
 import Products from "./components/Products";
 import ProductDetail from "./components/ProductDetail";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
-import InputForm from "./components/InputForm";
+import AddProductForm from "./components/AddProductForm";
 
 function App() {
   // This state use for set detail when click to product
@@ -10,20 +10,20 @@ function App() {
 
   return (
     <div className="App">
-      <Routes>
-        <Route
-          path="/"
-          element={<Products setProductDetail={setProductDetail} />}
-        />
-        <Route
-          path="/products/:productId"
-          element={<ProductDetail product={productDetail} />}
-        />
-        <Route
-          path="/form"
-          element={<InputForm  />}
-        />
-      </Routes>
+      {/** BrowserRouter declare that the project use routers */}
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Products setProductDetail={setProductDetail} />}
+          />
+          <Route
+            path="/products/:productId"
+            element={<ProductDetail product={productDetail} />}
+          />
+          <Route path="/addProduct" element={<AddProductForm />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
